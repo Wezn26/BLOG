94aa5e8f6a9529c50f66dd83e653bab8e73891ca
Leonid Second Comp <li_che_in@mail.ru>
2023-03-24 14:53:21 +0300

    Create App/Models/Main.php

---
+++ App/Models/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Models;
+
+/**
+ * Description of Main
+ *
+ * @author leonid
+ */
+class Main 
+{
+    protected const TABLE = 'posts';
+    public static $error;
+    
+    public static function contactValidate($post) 
+    {
+        $nameLenght = iconv_strlen($_POST['name']);
+        $textLenght = iconv_strlen($_POST['text']);
+        
+        if ($nameLenght < 2 || $nameLenght > 20) {
+            static::$error = 'Name must starting contain 2 till 20 symbols!!!';
+            return false;
+        } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            static::$error = 'Email is incorrect!!!';
+            return false;
+        } elseif ($textLenght < 10 || $textLenght > 500) {
+            static::$error = 'Message must starting contain 10 till 500 symbols!!!';
+            return false;
+        }
+        return true;
+    }
+    
+    public static function postsCount() 
+    {
+        $db = Db::give();
+        $sql = 'SELECT COUNT(id) FROM ' . static::TABLE;
+        return $db->column($sql);
+    }
+    
+    public static function postsList($route) 
+    {
+        $max = 2;
+        $db = Db::give();
+        $data = [
+            'max'   => $max,
+            'start' => ((($route['page'] ?? 1) - 1) * $max),
+        ];
+        
+        $sql = 'SELECT * FROM ' . static::TABLE . ' ORDER BY id DESC LIMIT :start, :max';
+        return $db->row($sql, $data);
+    }
+}
